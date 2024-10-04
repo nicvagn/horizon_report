5c0bff72e0449e6a2ea6c6b89b750dd82cfe6d0c
Nicolas Vaagen <nicolasvaagen@gmail.com>
2024-10-04 11:41:27 -0600

    added home app and html views

--- home/views.py
+++ home/views.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 
 # Create your views here.
 
+reports = ["report1", "report2"]
+
 
 def index(request):
-    return render(request, "home/index.html")
+    return render(request, "home/index.html", {"reports": reports})
