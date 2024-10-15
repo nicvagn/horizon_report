75d79898c11f0e9edb75bd7977bf848bbb6d237f
Nicolas Vaagen <nicolasvaagen@gmail.com>
2024-10-15 07:48:26 -0600

    skeliton

--- cfc_report/models.py
+++ cfc_report/models.py
@@ -1,3 +1,47 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+# models relaiting to a CFC Rated chess tournament.
+
+
+class Cfc_id(models.IntegerField):
+    """A CFC ID field"""
+    validators = [MinValueValidator(100000), MaxValueValidator(999999)]
+
+
+class Player(models.Model):
+    """A chess player"""
+    name = models.CharField(max_length=20)
+    cfc_id = Cfc_id()
+
+    def __str__(self):
+        return f"Player: {self.name} CFC: {self.cfc_id}"
+
+
+class TournamentDirector(models.Model):
+    """A tournament director for a chess tournament."""
+    name = models.CharField(max_length=20)
+    cfc_id = Cfc_id()
+
+    def __str__(self):
+        return f"Tournament Director: {self.name}, CFC: {self.cfc_id}"
+
+
+class TournamentOrganizer(models.Model):
+    """A tournament director for a chess tournament."""
+    name = models.CharField(max_length=20)
+    cfc_id = Cfc_id()
+
+    def __str__(self):
+        return f"Tournament Organizer: {self.name}, CFC: {self.cfc_id}"
+        
+class Tournament(models.Model):
+    """A CFC rated chess tournament"""
+    name = models.CharField(max_length=30)
+    num_rounds = models.IntegerField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f"""Tournament name: { self.name }
+        Number of rounds: { self.num_rounds }
+        date: {self.date}"""
--- cfc_report/views.py
+++ cfc_report/views.py
@@ -1,3 +1,66 @@
+"""views for cfc_report"""
+# horizon_pair
+# Copyright (C) 2024  Nicolas Vaagen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from django.shortcuts import render
 
-# Create your views here.
+from cfc.models import Player, TournamentDirector, TournamentOrganizer
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+reports = [{
+    "name": "report1",
+    "url": "report url"
+}, {
+    "name": "report2",
+    "url": "report2 url"
+}]
+
+
+def index(request):
+    """Main index page"""
+    player_list = Player.objects.all()
+    return render(
+        request, "home/index.html", {
+            "reports": reports,
+            "players": player_list,
+            "players_heading": "Player's in Database",
+        })
+
+def create_report(request):
+    """create a report for the cfc"""
+    players = Player.objects.all()
+    context = {"players": players}
+    return render(request, "create/index.html", context)
+    
+def view_report(request):
+    """display a CFC report"""
+    TO = TournamentOrganizer(name="Vlad the Impaler", cfc_id="123123")
+    TD = TournamentDirector(name="Bob Boy", cfc_id="4443434")
+
+    player_list = Player.objects.all()
+    num_players = len(player_list)
+    context = {
+        "title": "The Masters",
+        "province": "SK",
+        "time_format": "blitz",
+        "TD_cfc": TD.cfc_id,
+        "TO_cfc": TO.cfc_id,
+        "tournament_date": "9/11",
+        "players": player_list,
+        "num_players": num_players,
+    }
+    return render(request, "show/index.html", context)
