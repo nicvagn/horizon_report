235d221753d2a324c8de5ada88d36c8d16775ed1
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-03-14 19:49:44 -0600

    made django configurations changes to use it

--- horizon_report/asgi.py
+++ horizon_report/asgi.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "horizon_report.settings")
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
+
+from configurations.asgi import get_asgi_application
 
 application = get_asgi_application()
--- horizon_report/wsgi.py
+++ horizon_report/wsgi.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "horizon_report.settings")
 
+from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
