d9cf493bdb81c3cc5ca193f71387ce7a831d2fc3
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-03-14 22:06:14 -0600

    updated requirements.txt

--- horizon_report/wsgi.py
+++ horizon_report/wsgi.py
@@ -7,9 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+from configurations.wsgi import get_wsgi_application
 import os
 
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "horizon_report.settings")
 
-from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
