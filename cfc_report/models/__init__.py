2057b8c385f9419c920e594352c050ebfde4453d
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-03-16 18:12:27 -0600

    FIXUP

--- cfc_report/models/__init__.py
+++ cfc_report/models/__init__.py
@@ -1,4 +1,4 @@
-from .tournament import Roster, Match, Tournament
-from .fields import PairingSystemField, ProvinceField, CfcIdField
-from .person import Player, TournamentDirector, TournamentOrganizer
 from .etc import Province
+from .fields import CfcIdField, PairingSystemField, ProvinceField
+from .person import Player, TournamentDirector, TournamentOrganizer
+from .tournament import Match, Roster, Round, Tournament
