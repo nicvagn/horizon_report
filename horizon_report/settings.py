822ea57efb471b467400e5e84bf3806afbc26822
Nicolas Vaagen <nicolasvaagen@gmail.com>
2025-03-23 19:32:05 -0600

    WIP on MySQL db backend

--- horizon_report/settings.py
+++ horizon_report/settings.py
@@ -77,9 +77,11 @@ class Dev(Configuration):
     # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            "OPTIONS": {
+                "read_default_file": "~/horizon_report/horizon_report/db.cnf",
+            }
         }
     }
 
